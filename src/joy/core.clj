c0514a37ae970d06a772fdd8ea886bbad1dbbaf0
Jeff Bonhag <jeff@thebonhags.com>
2014-10-09 11:40:21 -0400

    Yes!

--- src/joy/core.clj
+++ src/joy/core.clj
@@ -4,10 +4,11 @@
 (defn -main 
   "I don't do a whole lot."
   [username password]
-  (def db2-db {:subprotocol "db2"
-                 :subname "//127.0.0.1:50000/SAMPLE"
-                 :user username
-                 :password password})
+  (def db2-db {:classname "com.ibm.db2.jcc.DB2Driver"
+               :subprotocol "db2"
+               :subname "//127.0.0.1:50000/SAMPLE"
+               :user username
+               :password password})
 
   (println
     (j/query db2-db
